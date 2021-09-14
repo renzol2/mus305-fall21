0fa80e49ecaf52a983664fb1d5e50355bd8234cc
Rick Taube <taube@illiacsoftware.com>
2021-09-14 09:48:50 -0500

    commit 4_ranchords

---
+++ 4_ranchords/ranchords.py
@@ -0,0 +1,11 @@
+
+def ranchords(num, low, high):
+    pass
+
+
+if __name__ == '__main__':
+
+    print([x for c in ranchords(22, 60, 80)])
+
+    # make a midi file that uses your ranchords()
+    # ...
