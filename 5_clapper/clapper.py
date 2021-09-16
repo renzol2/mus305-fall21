01ee900034dc123d22e529a3e39468fd99fa1b30
Rick Taube <taube@illiacsoftware.com>
2021-09-15 19:29:43 -0500

    commit 5_clapper

---
+++ 5_clapper/clapper.py
@@ -0,0 +1,11 @@
+
+def clapper(trope, repeat, wraparound=False):
+    pass
+
+
+if __name__ == '__main__':
+
+    print([x for x in clapper([1,2,3], 1)])
+
+    print([x for x in clapper("ABCD", 2, True)])
+
