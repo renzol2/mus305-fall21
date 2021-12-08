252c415c49ca204b6e682bc127d2c872b651850a
Renzo <renzol2@illinois.edu>
2021-12-08 14:30:29 -0600

    Add Markov chain generation

--- 12_finalproject/duyug_cr_1.py
+++ 12_finalproject/duyug_cr_1.py
@@ -1,25 +1,25 @@
 from phrases import Note
 
 # Opening
-DUYUG_CR_1_OPENING: list[Note] = [
+OPENING: list[Note] = [
     (3, 1/2, False)
 ] * 4
 
-# Submodules
-DUYUG_CR_1_SUBMODULE_1 = [
+# Subunits
+SUBUNIT_1 = [
     ((4, 1), 1/4, False),
     (1, 1/4, False),
     (5, 1/4, False),
     (3, 1/4, False)
 ]
 
-DUYUG_CR_1_SUBMODULE_2 = [
+SUBUNIT_2 = [
     (3, 1/2, False),
     (2, 1/4, False),
     (2, 1/4, False)
 ]
 
-DUYUG_CR_1_SUBMODULE_3 = [
+SUBUNIT_3 = [
     (3, 1/4, False),
     (2, 1/4, False),
     (3, 1/4, False),
@@ -27,29 +27,28 @@ DUYUG_CR_1_SUBMODULE_3 = [
 ]
 
 # Phrases
-DUYUG_CR_1_BODY_1: list[Note] = DUYUG_CR_1_SUBMODULE_1 + DUYUG_CR_1_SUBMODULE_2
+BODY_1: list[Note] = SUBUNIT_1 + SUBUNIT_2
 
-DUYUG_CR_1_BODY_2: list[Note] = [
+BODY_2: list[Note] = [
     (5, 1/4, False),
     (4, 1/4, False),
     (5, 1/4, False),
     (3, 1/4, False),
-] + DUYUG_CR_1_SUBMODULE_2
+] + SUBUNIT_2
 
-DUYUG_CR_1_BODY_3: list[Note] = DUYUG_CR_1_SUBMODULE_3 + DUYUG_CR_1_SUBMODULE_2
+BODY_3: list[Note] = SUBUNIT_3 + SUBUNIT_2
 
-DUYUG_CR_1_CLOSING: list[Note] = DUYUG_CR_1_SUBMODULE_3 + [
+CLOSING: list[Note] = SUBUNIT_3 + [
     (5, 1/4, False),
     (5, 1/4, False),
     (3, 1/4, False),
     ((4, 3), 1/4, False),
 ]
 
-DUYUG_CR_1_FINAL_NOTE: list[Note] = [
+FINAL_NOTE: list[Note] = [
     (5, 1, True)
 ]
 
 # Composition
-DUYUG_CR_1 = DUYUG_CR_1_OPENING + DUYUG_CR_1_BODY_1*3 + DUYUG_CR_1_BODY_2*3 + \
-    DUYUG_CR_1_BODY_1*3 + DUYUG_CR_1_BODY_3 * \
-    3 + DUYUG_CR_1_CLOSING + DUYUG_CR_1_FINAL_NOTE
+DUYUG_CR_1 = OPENING + BODY_1*3 + BODY_2*3 + \
+    BODY_1*3 + BODY_3*3 + CLOSING + FINAL_NOTE
--- 12_finalproject/kulintang.py
+++ 12_finalproject/kulintang.py
@@ -1,34 +1,20 @@
 import musx
 import pythonosc.udp_client
 import scosc  # in this script's directory
-from phrases import Note, DUYUG_CR_1_SAMPLE, DUYUG_A_BINALIG_C4_14_SAMPLE
+from phrases import Note
 from duyug_cr_1 import DUYUG_CR_1
 
 OSC_ADDRESS = '/musx/kulintang'
 
 
-def playvkey(score: musx.Score, scale: list[int], rules: list, rhy: float, dur: float, amp: float):
-    for gong in musx.all_rotations(scale, rules, False, True):
-        now = 0
-        # OSC MSG:   "pattern", scoretime, args...
-        # VKEY ARGS: keynum, start=0, dur=nil, amp=1, pan=0, out=0, ampenv=nil, bend=nil
-        msg = scosc.OscMessage(
-            OSC_ADDRESS, score.now+now, gong, 0, dur, amp)
-        score.add(msg)
-        now += rhy
-        yield now
-
-
-def plain_hunt():
-    return [[0, 2, 1], [1, 2, 1]]
-
-
-def compose_kulintang(score: musx.Score, notes: list[Note], dur: float, amp: float, tempo: int):
+def compose_kulintang(score: musx.Score, notes: list[Note], dur: float, amps: tuple[float, float], tempo: int):
     '''
     Adds a single kulintang notes to the given score
     '''
+    non_accent_amp, accent_amp = amps
     for gong_num, beat_length, accent in notes:
         now = 0
+        amp = accent_amp if accent else non_accent_amp
 
         # Add notes to score, depending on if there are 1-2 notes at the same time
         if isinstance(gong_num, int):
@@ -44,9 +30,20 @@ def compose_kulintang(score: musx.Score, notes: list[Note], dur: float, amp: flo
             ]
             for msg in msgs:
                 score.add(msg)
-        
+
         now += musx.intempo(beat_length, tempo)
         yield now
+
+
+def generate_pattern(original_pattern: list, order: int = musx.between(1, 4)):
+    rules = musx.markov_analyze(original_pattern, order)
+    gen = musx.markov(rules)
+    return [next(gen) for _ in range(len(original_pattern))]
+
+
+def pretty_print_kulintang_piece(notes: list[Note]):
+    for note in notes:
+        print(note)
 
 
 if __name__ == '__main__':
@@ -54,10 +51,13 @@ if __name__ == '__main__':
     print(oscout)
 
     dur = 0.5
-    amp = 0.5
+    amps = 0.5, 0.7
     tempo = 100
+
+    markov_kulintang_piece = generate_pattern(DUYUG_CR_1, 2)
+    pretty_print_kulintang_piece(markov_kulintang_piece)
 
     seq = musx.Seq()
     score = musx.Score(out=seq)
-    score.compose(compose_kulintang(score, DUYUG_CR_1, dur, amp, tempo))
+    score.compose(compose_kulintang(score, markov_kulintang_piece, dur, amps, tempo))
     scosc.oscplayer(seq, oscout)
